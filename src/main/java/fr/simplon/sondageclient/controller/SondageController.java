81baf7df2e35ef9fdaff3810d8cc4a6e27f99c33
Andy <121941283+Andytomsingvien@users.noreply.github.com>
2023-04-21 15:32:18 +0200

    Mon projet SondageClient ajout javadoc

--- src/main/java/fr/simplon/sondageclient/controller/SondageController.java
+++ src/main/java/fr/simplon/sondageclient/controller/SondageController.java
@@ -1,5 +1,7 @@
-package fr.simplon.sondageclient.controller;
+package fr.simplon.sondageclient.controller; /**
 
+ Le contrôleur SondageController gère les requêtes HTTP liées aux sondages.
+ */
 import fr.simplon.sondageclient.entity.Sondage;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -14,11 +16,18 @@ import org.springframework.http.*;
 
 import java.util.List;
 
+import java.util.List;
+
 @Controller
 public class SondageController {
-
     private RestTemplate restTemplate;
 
+    /**
+     * Affiche la page d'accueil contenant la liste de tous les sondages.
+     *
+     * @param model le modèle utilisé pour stocker les sondages à afficher
+     * @return la vue index affichant la liste des sondages
+     */
     @GetMapping("/")
     public String index(Model model){
         this.restTemplate = new RestTemplate();
@@ -30,6 +39,13 @@ public class SondageController {
         return "index";
     }
 
+    /**
+     * Affiche les détails d'un sondage donné.
+     *
+     * @param model le modèle utilisé pour stocker les détails du sondage à afficher
+     * @param id l'identifiant du sondage à afficher
+     * @return la vue sondage affichant les détails du sondage
+     */
     @GetMapping("/sondages/{id}")
     public String getSondage(Model model, @PathVariable long id){
         this.restTemplate = new RestTemplate();
@@ -42,6 +58,12 @@ public class SondageController {
         return "sondage";
     }
 
+    /**
+     * Affiche le formulaire pour ajouter un nouveau sondage.
+     *
+     * @param model le modèle utilisé pour stocker le nouveau sondage à ajouter
+     * @return la vue form pour ajouter un nouveau sondage
+     */
     @GetMapping("/sondages/form/add")
     public String formSondage(Model model)
     {
@@ -50,10 +72,17 @@ public class SondageController {
         return "form";
     }
 
+    /**
+     * Affiche le formulaire pour modifier un sondage donné.
+     *
+     * @param model le modèle utilisé pour stocker le sondage à modifier
+     * @param id l'identifiant du sondage à modifier
+     * @return la vue edit pour modifier le sondage
+     */
     @GetMapping("/sondages/maj/{id}")
     public String majSondage(Model model, @PathVariable long id)
     {
-            this.restTemplate = new RestTemplate();
+        this.restTemplate = new RestTemplate();
         String url = "http://localhost:8080/rest/sondages/{id}";
         ResponseEntity<Sondage> response = restTemplate.getForEntity(url, Sondage.class, id);
         Sondage sondage = response.getBody();
@@ -61,20 +90,30 @@ public class SondageController {
         model.addAttribute("sondage", sondage);
         return "edit";
     }
-
-
-
+    /**
+     * Supprime un sondage donné.
+     *
+     * @param model le modèle utilisé pour stocker les informations de redirection après suppression du sondage
+     * @param id l'identifiant du sondage à supprimer
+     * @return la vue index redirigeant vers la liste des sondages
+     */
     @PostMapping("/sondages/del/{id}")
     public String delSondage(Model model, @PathVariable long id)
     {
         this.restTemplate = new RestTemplate();
+    String url = "http://localhost:8080/rest/sondages/{id}";
+    restTemplate.delete(url, id);
 
-        String url = "http://localhost:8080/rest/sondages/{id}";
-        restTemplate.delete(url, id);
+    return "redirect:/";
+}
 
-        return "redirect:/";
-    }
-
+    /**
+     * Modifie les informations d'un sondage donné.
+     *
+     * @param sondage le sondage modifié
+     * @param id l'identifiant du sondage à modifier
+     * @return la vue index redirigeant vers la liste des sondages
+     */
     @PostMapping("/sondages/maj/{id}")
     public String updateFruit(@ModelAttribute("sondage") Sondage sondage, @PathVariable long id){
 
@@ -87,6 +126,12 @@ public class SondageController {
         return "redirect:/";
     }
 
+    /**
+     * Ajoute un nouveau sondage.
+     *
+     * @param sondage le nouveau sondage à ajouter
+     * @return la vue index redirigeant vers la liste des sondages
+     */
     @PostMapping("/sondages/form/add")
     public String addSondage(@ModelAttribute("sondage") Sondage sondage){
         this.restTemplate = new RestTemplate();
@@ -99,5 +144,4 @@ public class SondageController {
 
         return "redirect:/";
     }
-
 }
